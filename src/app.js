032f4bb442ade9fb90881840d79646c6f1c4cbfe
Aman Sahni <119403533+Itsmekng@users.noreply.github.com>
2024-03-01 22:02:37 -1200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -31,11 +31,11 @@ import  Portfolio  from "./routes/project.routes.js";
 
 // app.use("/fanscart",trial)
 
-app.use("/portfolio",User)
+app.use("/api/portfolio",User)
 
-app.use("/portfolio",ContactUs)
+app.use("/api/portfolio",ContactUs)
 
-app.use("/portfolio",Portfolio)
+app.use("/api/portfolio",Portfolio)
 
 
 
